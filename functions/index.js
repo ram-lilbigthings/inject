a53ef277a21a6ad8d7d22a0edb9120475a767696
sram1112002 <ram@littlebigthings.dev>
2023-05-31 10:47:26 +0530

    SEO

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
   
     try {
       const idValue = await env.KV.get(siteName);
-      
+  
       if (idValue) {
         const scriptCode = '<script id="' + idValue + '"></script>';
         const response = new Response(scriptCode, {
@@ -23,11 +23,11 @@ export async function onRequest(context) {
         return response;
       } else {
         console.log('ID value not found');
-        return new Response('ID value not found');
+        return new Response('ID value not found', { status: 404 });
       }
     } catch (error) {
       console.error(error);
-      return new Response('Error occurred');
+      return new Response('Error occurred', { status: 500 });
     }
   }
   
