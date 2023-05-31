ff146bc203689e804e725ddff2b8782f17aa7928
sram1112002 <ram@littlebigthings.dev>
2023-05-31 09:01:51 +0530

    SEO

--- workers.js
+++ workers.js
@@ -1,6 +1,6 @@
 export async function handleRequest(request) {
     const siteName = request.url.split('/').pop();
-    const idValue = await TODO_LIST.get(siteName);
+    const idValue = await KV.get(siteName);
   
     if (idValue) {
       const scriptCode = `<script id="${idValue}"></script>`;
