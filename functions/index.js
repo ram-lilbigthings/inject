dbb6d9add092551d08e37c17a72f652e1775438e
sram1112002 <ram@littlebigthings.dev>
2023-05-31 10:44:54 +0530

    SEO

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,33 @@
-
 export async function onRequest(context) {
     const {
-        request, // same as existing Worker API
-        env, // same as existing Worker API
-        params, // if filename includes [id] or [[path]]
-        waitUntil, // same as ctx.waitUntil in existing Worker API
-        next, // used for middleware or to fetch assets
-        data, // arbitrary space for passing data between middlewares
-      } = context;
+      request,
+      env,
+      params,
+      waitUntil,
+      next,
+      data,
+    } = context;
+  
     const { pathname } = new URL(request.url);
     const siteName = pathname.split('/').pop();
-    console.log(siteName)
-    const idValue = await env.KV.get(siteName);
+    console.log(siteName);
+  
     try {
-        if (idValue) {
-            const scriptCode = '<script id="' + idValue + '"></script>';
-            const response = new Response(scriptCode, {
-              headers: { 'Content-Type': 'text/html' },
-            });
-            return response;
-        } else {
-            console.log('ID value not found');
-            return new Response('ID value not found');
-        }
-      } catch (error) {
-        console.error(error);
-      return new Response('Error occurred');
+      const idValue = await env.KV.get(siteName);
+      
+      if (idValue) {
+        const scriptCode = '<script id="' + idValue + '"></script>';
+        const response = new Response(scriptCode, {
+          headers: { 'Content-Type': 'text/html' },
+        });
+        return response;
+      } else {
+        console.log('ID value not found');
+        return new Response('ID value not found');
       }
+    } catch (error) {
+      console.error(error);
+      return new Response('Error occurred');
+    }
   }
+  
